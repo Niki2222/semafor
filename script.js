083ba72e01dfc9d0220188449fb99856b36acb6a
Niki2222 <89404731+Niki2222@users.noreply.github.com>
2023-12-15 18:18:31 +0200

    commit
    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let countColors = 0;
+let myButton = document.getElementById('initial-button');
+function changeColour() {
+    ++countColors;
+    if (countColors % 3 == 0) {
+        myButton.style.backgroundColor = 'red';
+    } else if (countColors % 3 == 1) {
+        myButton.style.backgroundColor = 'yellow';
+    } else {
+        myButton.style.backgroundColor = 'green';;
+    }
+}
